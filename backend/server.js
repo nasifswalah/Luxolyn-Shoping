ac4a54de8d76d7e825bd900a39233f06e7660adf
nasifswalah <nasifswalah@gmail.com>
2024-09-27 08:48:42 +0530

    Authentication setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+import authRoutes from './routes/auth.routes.js';
 
 import { connectDB } from './lib/db.js';
 
@@ -7,6 +10,11 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/auth',authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
