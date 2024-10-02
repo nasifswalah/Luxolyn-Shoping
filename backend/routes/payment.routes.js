529588741cd4b644c9a44e57ef8a44987018588b
nasifswalah <nasifswalah@gmail.com>
2024-10-02 17:15:07 +0530

    completed the project

--- backend/routes/payment.routes.js
+++
@@ -1,8 +0,0 @@
-// import express from "express";
-// import { protectRoute } from "../middlewares/auth.middleware.js";
-
-// const router = express.Router();
-
-// router.get('/create-chekout-session', protectRoute);
-
-// export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import path from 'path'
+
 
 import authRoutes from './routes/auth.routes.js';
 import productRoute from './routes/product.route.js';
 import cartRoute from './routes/cart.route.js';
-// import paymentRoute from "./routes/payment.routes.js";
+
 
 import { connectDB } from './lib/db.js';
 
@@ -15,13 +16,22 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth',authRoutes);
 app.use('/api/products', productRoute);
 app.use('/api/cart', cartRoute);
-// app.use('/api/payments', paymentRoute);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
